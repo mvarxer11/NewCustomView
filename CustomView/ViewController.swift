93f46a2d877643f6874005df626d9df7fd77641d
mvarxer <mvarxer@163.com>
2017-02-25 21:11:41 +0800

    first commit

--- CustomView/ViewController.swift
+++ CustomView/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var baseView: UIView!
+    var ttt:TestView! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //纯代码创建TTTView
+        ttt = TestView(frame: CGRect(x: 0, y: 10, width: baseView.bounds.width , height: baseView.bounds.height*0.4))
+        baseView.addSubview(ttt)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+@IBDesignable
+class TestView:UIView {
+    
+    
+    
+    
+    override func draw(_ rect: CGRect) {
+        print("draw--------------------------")
+        print(self.frame)
+    }
+    
+    override func layoutSubviews() {
+        print("layoutSubviews--------------")
+        print(self.frame)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .yellow
+        print("init(frame:---------------")
+        print(self.frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = .blue
+        print("init?(coder---------------")
+        print(self.frame)
+    }
+    
+    
+    
+}
+
 
